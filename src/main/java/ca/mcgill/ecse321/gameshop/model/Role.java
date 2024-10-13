0ae85edb4c0c44cd9ed7e9083330bf6d936f34e4
Joseph <126432570+josephroustom@users.noreply.github.com>
2024-10-13 11:15:08 -0400

    Update Role.java

--- src/main/java/ca/mcgill/ecse321/gameshop/model/Role.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/model/Role.java
@@ -1,62 +1,80 @@
 package ca.mcgill.ecse321.gameshop.model;
 
-//%% NEW FILE Role BEGINS HERE %%
 
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
-
-
+import jakarta.persistence.*;
 
 /**
  * Abstract Role class
  */
-// line 11 "model.ump"
-// line 96 "model.ump"
-public abstract class Role
-{
+// line 12 "model.ump"
+// line 101 "model.ump"
+@MappedSuperclass
+public abstract class Role {
 
-    //------------------------
-    // MEMBER VARIABLES
-    //------------------------
+  @Id
+  @GeneratedValue
+  private int roleId;
+  //Role Associations
+  @ManyToOne
+  private Person person;
+  //------------------------
 
-    //Role Associations
-    private Person person;
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
 
-    //------------------------
-    // CONSTRUCTOR
-    //------------------------
-
-    public Role(Person aPerson)
+  public Role(int aRoleId, Person aPerson)
+  {
+    roleId = aRoleId;
+    if (!setPerson(aPerson))
     {
-        if (!setPerson(aPerson))
-        {
-            throw new RuntimeException("Unable to create Role due to aPerson. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
-        }
+      throw new RuntimeException("Unable to create Role due to aPerson. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
     }
+  }
 
-    //------------------------
-    // INTERFACE
-    //------------------------
-    /* Code from template association_GetOne */
-    public Person getPerson()
-    {
-        return person;
-    }
-    /* Code from template association_SetUnidirectionalOne */
-    public boolean setPerson(Person aNewPerson)
-    {
-        boolean wasSet = false;
-        if (aNewPerson != null)
-        {
-            person = aNewPerson;
-            wasSet = true;
-        }
-        return wasSet;
-    }
+  //------------------------
+  // INTERFACE
+  //------------------------
 
-    public void delete()
-    {
-        person = null;
-    }
+  public boolean setRoleId(int aRoleId)
+  {
+    boolean wasSet = false;
+    roleId = aRoleId;
+    wasSet = true;
+    return wasSet;
+  }
 
+  public int getRoleId()
+  {
+    return roleId;
+  }
+  /* Code from template association_GetOne */
+  public Person getPerson()
+  {
+    return person;
+  }
+  /* Code from template association_SetUnidirectionalOne */
+  public boolean setPerson(Person aNewPerson)
+  {
+    boolean wasSet = false;
+    if (aNewPerson != null)
+    {
+      person = aNewPerson;
+      wasSet = true;
+    }
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    person = null;
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "roleId" + ":" + getRoleId()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "person = "+(getPerson()!=null?Integer.toHexString(System.identityHashCode(getPerson())):"null");
+  }
 }
