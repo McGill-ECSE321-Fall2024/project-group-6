9e170523aee48f414c8b943dfb4eba06b187d5c6
Joseph <126432570+josephroustom@users.noreply.github.com>
2024-10-13 11:13:19 -0400

    Update Person.java

--- src/main/java/ca/mcgill/ecse321/gameshop/model/Person.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/model/Person.java
@@ -1,139 +1,156 @@
 package ca.mcgill.ecse321.gameshop.model;
 
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.34.0.7242.6b8819789 modeling language!*/
-
-
 import java.util.*;
-
+import jakarta.persistence.*;
 /**
  * Defining the Person class
  */
 // line 3 "model.ump"
-// line 174 "model.ump"
-public class Person
-{
+// line 179 "model.ump"
+@Entity
+public class Person{
 
-    //------------------------
-    // STATIC VARIABLES
-    //------------------------
+  //------------------------
+  // STATIC VARIABLES
+  //------------------------
 
-    private static Map<String, Person> personsByEmail = new HashMap<String, Person>();
+  private static Map<String, Person> personsByEmail = new HashMap<String, Person>();
 
-    //------------------------
-    // MEMBER VARIABLES
-    //------------------------
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
 
-    //Person Attributes
-    private String username;
-    private String email;
-    private String password;
-    private String phone;
+  //Person Attributes
+  private String username;
+  @Column(unique = true)
+  private String email;
+  private String password;
+  private String phone;
+  @Id
+ @GeneratedValue
+  private int userId;
 
-    //------------------------
-    // CONSTRUCTOR
-    //------------------------
 
-    public Person(String aUsername, String aEmail, String aPassword, String aPhone)
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Person(String aUsername, String aEmail, String aPassword, String aPhone, int aUserId)
+  {
+    username = aUsername;
+    password = aPassword;
+    phone = aPhone;
+    userId = aUserId;
+    if (!setEmail(aEmail))
     {
-        username = aUsername;
-        password = aPassword;
-        phone = aPhone;
-        if (!setEmail(aEmail))
-        {
-            throw new RuntimeException("Cannot create due to duplicate email. See https://manual.umple.org?RE003ViolationofUniqueness.html");
-        }
+      throw new RuntimeException("Cannot create due to duplicate email. See https://manual.umple.org?RE003ViolationofUniqueness.html");
     }
+  }
 
-    //------------------------
-    // INTERFACE
-    //------------------------
+  //------------------------
+  // INTERFACE
+  //------------------------
 
-    public boolean setUsername(String aUsername)
-    {
-        boolean wasSet = false;
-        username = aUsername;
-        wasSet = true;
-        return wasSet;
-    }
+  public boolean setUsername(String aUsername)
+  {
+    boolean wasSet = false;
+    username = aUsername;
+    wasSet = true;
+    return wasSet;
+  }
 
-    public boolean setEmail(String aEmail)
-    {
-        boolean wasSet = false;
-        String anOldEmail = getEmail();
-        if (anOldEmail != null && anOldEmail.equals(aEmail)) {
-            return true;
-        }
-        if (hasWithEmail(aEmail)) {
-            return wasSet;
-        }
-        email = aEmail;
-        wasSet = true;
-        if (anOldEmail != null) {
-            personsByEmail.remove(anOldEmail);
-        }
-        personsByEmail.put(aEmail, this);
-        return wasSet;
+  public boolean setEmail(String aEmail)
+  {
+    boolean wasSet = false;
+    String anOldEmail = getEmail();
+    if (anOldEmail != null && anOldEmail.equals(aEmail)) {
+      return true;
     }
+    if (hasWithEmail(aEmail)) {
+      return wasSet;
+    }
+    email = aEmail;
+    wasSet = true;
+    if (anOldEmail != null) {
+      personsByEmail.remove(anOldEmail);
+    }
+    personsByEmail.put(aEmail, this);
+    return wasSet;
+  }
 
-    public boolean setPassword(String aPassword)
-    {
-        boolean wasSet = false;
-        password = aPassword;
-        wasSet = true;
-        return wasSet;
-    }
+  public boolean setPassword(String aPassword)
+  {
+    boolean wasSet = false;
+    password = aPassword;
+    wasSet = true;
+    return wasSet;
+  }
 
-    public boolean setPhone(String aPhone)
-    {
-        boolean wasSet = false;
-        phone = aPhone;
-        wasSet = true;
-        return wasSet;
-    }
+  public boolean setPhone(String aPhone)
+  {
+    boolean wasSet = false;
+    phone = aPhone;
+    wasSet = true;
+    return wasSet;
+  }
 
-    public String getUsername()
-    {
-        return username;
-    }
+  public boolean setUserId(int aUserId)
+  {
+    boolean wasSet = false;
+    userId = aUserId;
+    wasSet = true;
+    return wasSet;
+  }
 
-    public String getEmail()
-    {
-        return email;
-    }
-    /* Code from template attribute_GetUnique */
-    public static Person getWithEmail(String aEmail)
-    {
-        return personsByEmail.get(aEmail);
-    }
-    /* Code from template attribute_HasUnique */
-    public static boolean hasWithEmail(String aEmail)
-    {
-        return getWithEmail(aEmail) != null;
-    }
+  public String getUsername()
+  {
+    return username;
+  }
 
-    public String getPassword()
-    {
-        return password;
-    }
+  public String getEmail()
+  {
+    return email;
+  }
+  /* Code from template attribute_GetUnique */
+  public static Person getWithEmail(String aEmail)
+  {
+    return personsByEmail.get(aEmail);
+  }
+  /* Code from template attribute_HasUnique */
+  public static boolean hasWithEmail(String aEmail)
+  {
+    return getWithEmail(aEmail) != null;
+  }
 
-    public String getPhone()
-    {
-        return phone;
-    }
+  public String getPassword()
+  {
+    return password;
+  }
 
-    public void delete()
-    {
-        personsByEmail.remove(getEmail());
-    }
+  public String getPhone()
+  {
+    return phone;
+  }
+
+  public int getUserId()
+  {
+    return userId;
+  }
+
+  public void delete()
+  {
+    personsByEmail.remove(getEmail());
+  }
 
 
-    public String toString()
-    {
-        return super.toString() + "["+
-                "username" + ":" + getUsername()+ "," +
-                "email" + ":" + getEmail()+ "," +
-                "password" + ":" + getPassword()+ "," +
-                "phone" + ":" + getPhone()+ "]";
-    }
+  public String toString()
+  {
+    return super.toString() + "["+
+            "username" + ":" + getUsername()+ "," +
+            "email" + ":" + getEmail()+ "," +
+            "password" + ":" + getPassword()+ "," +
+            "phone" + ":" + getPhone()+ "," +
+            "userId" + ":" + getUserId()+ "]";
+  }
 }
