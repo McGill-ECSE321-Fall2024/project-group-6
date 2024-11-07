4eacb55a07afe12de03b4bad1909a5d25069df75
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2024-11-07 01:58:18 -0500

    Update GamesResponseDTO.java

--- src/main/java/ca/mcgill/ecse321/gameshop/dto/GamesResponseDTO.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/dto/GamesResponseDTO.java
@@ -1,1 +1,19 @@
+package ca.mcgill.ecse321.gameshop.dto;
+
+import java.util.List;
+public class GamesResponseDTO {
+    private List<GameResponseDTO> games;
+
+    public GamesResponseDTO(List<GameResponseDTO> games) {
+        this.games = games;
+    }
+
+    public List<GameResponseDTO> getGames() {
+        return games;
+    }
+
+    public void setRoles(List<GameResponseDTO> games) {
+        this.games = games;
+    }
+}
 
