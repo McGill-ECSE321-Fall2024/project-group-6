e2dc6fcfc6a50b2ed1acb2cea0752c8001589464
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2024-11-07 01:54:17 -0500

    Update RoleController.java

--- src/main/java/ca/mcgill/ecse321/gameshop/controller/RoleController.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/controller/RoleController.java
@@ -1,12 +1,12 @@
 package ca.mcgill.ecse321.gameshop.controller;
 
-import ca.mcgill.ecse321.gameshop.dto.RoleDTO;
+import ca.mcgill.ecse321.gameshop.dto.*;
+import ca.mcgill.ecse321.gameshop.model.*;
 import ca.mcgill.ecse321.gameshop.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,39 +16,37 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
-    // Create a new role
-    @PostMapping
-    public ResponseEntity<RoleDTO> createRole(@RequestBody RoleDTO roleDTO) {
-        RoleDTO createdRole = roleService.createRole(roleDTO);
-        return new ResponseEntity<>(createdRole, HttpStatus.CREATED);
-    }
-
-    // Get all roles
     @GetMapping
-    public ResponseEntity<List<RoleDTO>> getAllRoles() {
-        List<RoleDTO> roles = roleService.getAllRoles();
-        return new ResponseEntity<>(roles, HttpStatus.OK);
+    public RolesResponseDTO getAllRoles() {
+        List<Role> roles = roleService.getAllRoles();
+        List<RoleResponseDTO> responseList = new ArrayList<>();
+        for (Role r : roles){
+            RoleResponseDTO response = new RoleResponseDTO(r);
+            responseList.add(response);
+        }
+        return new RolesResponseDTO(responseList);
     }
 
-    // Get a specific role by ID
     @GetMapping("/{id}")
-    public ResponseEntity<RoleDTO> getRoleById(@PathVariable int id) {
-        RoleDTO role = roleService.getRoleById(id);
-        return role != null ? new ResponseEntity<>(role, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public RoleResponseDTO getRoleById(@PathVariable int id) {
+        Role role = roleService.getRoleById(id);
+        return new RoleResponseDTO(role);
     }
 
-    // Update an existing role
+    @PostMapping("/addRole")
+    public RoleResponseDTO createRole(@RequestBody RoleRequestDTO role) {
+        Role createdRole = new Role(role.getPerson());
+        return new RoleResponseDTO(createdRole);
+    }
+
     @PutMapping("/{id}")
-    public ResponseEntity<RoleDTO> updateRole(@PathVariable int id, @RequestBody RoleDTO roleDTO) {
-        RoleDTO updatedRole = roleService.updateRole(id, roleDTO);
-        return updatedRole != null ? new ResponseEntity<>(updatedRole, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public RoleResponseDTO updateRole(@PathVariable int id, @RequestBody Person person) {
+        Role updatedRole = roleService.updateRole(id, person);
+        return new RoleResponseDTO(updatedRole);
     }
 
-    // Delete a role
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteRole(@PathVariable int id) {
-        boolean deleted = roleService.deleteRole(id);
-        return deleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public void deleteRole(@PathVariable int id) {
+        roleService.deleteRole(id);
     }
 }
-
