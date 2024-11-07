bd8aacd05639d4b83a9b50c5b8487ceb9ec9cb0d
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2024-11-07 01:59:09 -0500

    Update RoleResponseDTO.java

--- src/main/java/ca/mcgill/ecse321/gameshop/dto/RoleResponseDTO.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/dto/RoleResponseDTO.java
@@ -1,1 +1,34 @@
+package ca.mcgill.ecse321.gameshop.dto;
+
+import ca.mcgill.ecse321.gameshop.model.Person;
+import ca.mcgill.ecse321.gameshop.model.Role;
+
+public class RoleResponseDTO {
+    private int roleId;
+    private Person person;
+
+    public RoleResponseDTO() {
+    }
+
+    public RoleResponseDTO(Role role) {
+        this.roleId = role.getRoleId();
+        this.person = role.getPerson();
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson (Person person) {
+        this.person = person;
+    }
+}
 
