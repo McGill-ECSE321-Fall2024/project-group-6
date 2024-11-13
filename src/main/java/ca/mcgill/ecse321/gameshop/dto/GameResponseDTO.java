2fabba0b3b1796ba4da01468dd0c056cf2406481
LaurencePerreault <143827146+LaurencePerreault@users.noreply.github.com>
2024-11-12 22:10:09 -0500

    Update GameResponseDTO.java

--- src/main/java/ca/mcgill/ecse321/gameshop/dto/GameResponseDTO.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/dto/GameResponseDTO.java
@@ -5,19 +5,27 @@ import ca.mcgill.ecse321.gameshop.model.*;
 import java.util.Collections;
 import java.util.List;
 
-public class GameResponseDTO {
+
+public class GameResponseDto {
+
     private String name;
     private String description;
     private float price;
     private int stockQuantity;
+
     private String photoURL;
+    private int gameId;
     private boolean toBeAdded;
+
     private boolean toBeRemoved;
     private float promotion;
-    private List<Category> categories;
-    private int gameId;
 
-    public GameResponseDTO(Game game) {
+    private List<Category> categories;
+
+    public GameResponseDto() {
+    }
+    public GameResponseDto(Game game) {
+        this.gameId=game.getGameId();
         this.description = game.getDescription();
         this.name = game.getName();
         this.photoURL = game.getPhotoURL();
@@ -26,81 +34,111 @@ public class GameResponseDTO {
         this.toBeAdded = game.getToBeAdded();
         this.toBeRemoved = game.getToBeRemoved();
         this.stockQuantity = game.getStockQuantity();
-        this.categories = game.getCategories();
-        this.gameId = game.getGameId();
+        this.categories=game.getCategories();
     }
+
+    public boolean setName(String aName) {
+        boolean wasSet = false;
+        name = aName;
+        wasSet = true;
+        return wasSet;
+    }
+
+
+    public boolean setDescription(String aDescription) {
+        boolean wasSet = false;
+        description = aDescription;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setPrice(float aPrice) {
+        boolean wasSet = false;
+        price = aPrice;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setStockQuantity(int aStockQuantity) {
+        boolean wasSet = false;
+        stockQuantity = aStockQuantity;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setPhotoURL(String aPhotoURL) {
+        boolean wasSet = false;
+        photoURL = aPhotoURL;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setGameId(int aGameId) {
+        boolean wasSet = false;
+        gameId = aGameId;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setToBeAdded(boolean aToBeAdded) {
+        boolean wasSet = false;
+        toBeAdded = aToBeAdded;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setToBeRemoved(boolean aToBeRemoved) {
+        boolean wasSet = false;
+        toBeRemoved = aToBeRemoved;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setPromotion(float aPromotion) {
+        boolean wasSet = false;
+        promotion = aPromotion;
+        wasSet = true;
+        return wasSet;
+    }
+
     public String getName() {
         return name;
-    }
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     public float getPrice() {
         return price;
-    }
-    public void setPrice(float price) {
-        this.price = price;
     }
 
     public int getStockQuantity() {
         return stockQuantity;
     }
 
-    public void setStockQuantity(int stockQuantity) {
-        this.stockQuantity = stockQuantity;
-    }
-
     public String getPhotoURL() {
         return photoURL;
-    }
-
-    public void setPhotoURL(String photoURL) {
-        this.photoURL = photoURL;
-    }
-
-    public boolean getToBeAdded() {
-        return toBeAdded;
-    }
-    public void setToBeAdded(boolean toBeAdded) {
-        this.toBeAdded = toBeAdded;
-    }
-
-    public boolean getToBeRemoved() {
-        return toBeRemoved;
-    }
-
-    public void setToBeRemoved(boolean toBeRemoved) {
-        this.toBeRemoved = toBeRemoved;
-    }
-
-    public float getPromotion() {
-        return promotion;
-    }
-    public void setPromotion(float promotion) {
-        this.promotion = promotion;
-    }
-    public List<Category> getCategories() {
-        List<Category> newCategories = Collections.unmodifiableList(categories);
-        return newCategories;
-    }
-
-    public void setCategories(List<Category> categories) {
-        this.categories = categories;
     }
 
     public int getGameId() {
         return gameId;
     }
 
-    public void setGameId(int id) {
-        this.gameId = id;
+    public boolean getToBeAdded() {
+        return toBeAdded;
+    }
+
+    public boolean getToBeRemoved() {
+        return toBeRemoved;
+    }
+
+    public float getPromotion() {
+        return promotion;
+    }
+    public List<Category> getCategories()
+    {
+        List<Category> newCategories = Collections.unmodifiableList(categories);
+        return newCategories;
     }
 }
