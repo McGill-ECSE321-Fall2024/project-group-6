0ab43bc0255f63b2671ab453ce577219020e4c31
mdupuy2002 <marine.dupuy@mail.mcgill.ca>
2024-11-25 11:26:19 -0500

    Employee Dashboard, Account and Game Page vue files (not linked to backend)

---
+++ GameShop-FrontEnd/src/router/index.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventDetailsView from '../views/EventDetailsView.vue'
+import EventsView from '../views/EventsView.vue'
+import SignUp from '@/views/SignUp.vue'
+import SignIn from '@/views/SignIn.vue'
+import HomePageView from '@/views/HomePageView.vue'
+import EmployeeHome from '@/views/EmployeeHome.vue'   // Add employee Home component
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'events',
+      component: SignUp
+    },
+    {
+      path: '/homepage',
+      name: 'event-details',
+      component: HomePageView
+    },
+   
+    {
+      path: '/hello',
+      name: 'home',
+      component: SignIn,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/employee',
+      name: 'employee-dashboard', // Ensure this matches SignIn.vue
+      component: EmployeeHome, // Points to EmployeeHome.vue
+      meta: { requiresAuth: true, role: 'employee' },
+    },
+    {
+      path: '/game/:id',
+      name: 'game-details',
+      component: () => import('@/views/GameDetails.vue'),
+      meta: { requiresAuth: true, role: 'employee' },
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('@/views/EmployeeAccount.vue'),
+      meta: { requiresAuth: true },
+    }
+  ],
+})
+
+export default router
