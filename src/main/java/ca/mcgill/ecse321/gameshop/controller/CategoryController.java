a75ddab75d7889c5b076066162752403c52d4033
Mario Ghorayeb <mario_g8@live.com>
2024-12-01 20:05:47 -0500

    Manager Home Page

--- src/main/java/ca/mcgill/ecse321/gameshop/controller/CategoryController.java
+++ src/main/java/ca/mcgill/ecse321/gameshop/controller/CategoryController.java
@@ -19,7 +19,6 @@ import ca.mcgill.ecse321.gameshop.model.Category;
 import ca.mcgill.ecse321.gameshop.service.CategoryService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8087")
 public class CategoryController {
 
     @Autowired
@@ -30,6 +29,7 @@ public class CategoryController {
      * @param c
      * @return
      */
+    @CrossOrigin(origins = "http://localhost:8087")
     @PostMapping("/categories")
     public CategoryResponseDto createCategory(@RequestBody CategoryRequestDto c){
 
@@ -44,6 +44,7 @@ public class CategoryController {
      * @param name
      * @return
      */
+    @CrossOrigin(origins = "http://localhost:8087")
     @PutMapping("/categories/{ID}")
     public CategoryResponseDto updateCategory(@PathVariable int ID, @RequestBody String name){
         return new CategoryResponseDto(categoryService.updateCategory(ID,name));
@@ -54,6 +55,7 @@ public class CategoryController {
      * @param ID
      * @return
      */
+    @CrossOrigin(origins = "http://localhost:8087")
     @GetMapping("/categories/{ID}")
     public CategoryResponseDto findCategoryById(@PathVariable int ID){
         return new CategoryResponseDto(categoryService.findCategoryById(ID));
@@ -63,6 +65,7 @@ public class CategoryController {
      * Get all categories
      * @return
      */
+    @CrossOrigin(origins = "http://localhost:8087")
     @GetMapping("/categories")
     public CategoryListDto getAllCategories() {
         List<CategoryResponseDto> categories = new ArrayList<>();
@@ -76,6 +79,7 @@ public class CategoryController {
      * Delete a category
      * @param ID
      */
+    @CrossOrigin(origins = "http://localhost:8087")
     @DeleteMapping("/categories/{ID}")
     public void deleteCategory(@PathVariable int ID) {
         categoryService.deleteCategory(ID);
